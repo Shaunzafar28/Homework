eb02b338053518fc0e348f763a9bcdc559d11c83
Shaunzafar28 <55443382+Shaunzafar28@users.noreply.github.com>
2019-09-19 08:34:45 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,22 @@
+// Shaun Zafar
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name: Shaun Zafar" << std::endl
+		<< "Period 1: Computer Science" << std::endl
+		<< "Period 2: Global History" << std::endl
+		<< "Period 3: Robotics" << std::endl
+		<< "Period 4: Health" << std::endl
+		<< "Period 5: Lunch" << std::endl
+		<< "Period 6: ELA Honors" << std::endl
+		<< "Period 7: House" << std::endl
+		<< "Period 8: Algebra 2" << std::endl
+		<< "Period 9: Forensics" << std::endl
+		<< "Period 10: Gym" << std::endl;
+}
