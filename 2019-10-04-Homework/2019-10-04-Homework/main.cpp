0379e31e482bac69317db6bc1026a61b7d7a7ee5
Shaunzafar28 <55443382+Shaunzafar28@users.noreply.github.com>
2019-10-10 20:09:00 -0400

    Add files via upload

---
+++ 2019-10-04-Homework/2019-10-04-Homework/main.cpp
@@ -0,0 +1,26 @@
+//Shaun Zafar
+//Intro to CS
+//Mr.Williams
+//Period 1
+//2019-10-04-Homework
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float p; p =(2 * 5.5 * 3.14);
+		float a; a = (5.5 * 5.5 * 3.14);
+		cout << "Perimeter and Area of Circle" << endl;
+		cout << "Perimeter: "<< p  << endl;
+		cout << "Area: "<< a << endl;
+
+		
+		float Peri; Peri= (2 * 4.5) + (2 * 7.9);
+		float Area; Area= (4.5 * 7.9);
+		cout << "Perimeter and Area of Rectangle" << endl;
+		cout << "Perimeter: " << Peri << endl;
+		cout << "Area: " << Area << endl;
+
+
+}
